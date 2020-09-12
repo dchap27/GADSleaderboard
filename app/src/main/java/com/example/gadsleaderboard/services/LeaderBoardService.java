423dc87dab2d246aa15d3ad7bc8ba64b1e5cfd4f
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-09-12 08:36:59 +0100

    Add services package

--- app/src/main/java/com/example/gadsleaderboard/services/LeaderBoardService.java
+++ app/src/main/java/com/example/gadsleaderboard/services/LeaderBoardService.java
@@ -1,4 +1,17 @@
 package com.example.gadsleaderboard.services;
 
+import com.example.gadsleaderboard.LearnersInfo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface LeaderBoardService {
+
+    @GET("hours")
+    Call<List<LearnersInfo>> getTopLearners();
+
+    @GET("skilliq")
+    Call<List<LearnersInfo>> getTopSkills();
 }
--- app/src/main/java/com/example/gadsleaderboard/services/ServiceBuilder.java
+++ app/src/main/java/com/example/gadsleaderboard/services/ServiceBuilder.java
@@ -1,4 +1,27 @@
 package com.example.gadsleaderboard.services;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ServiceBuilder {
+    private static final String URL = "https://gadsapi.herokuapp.com/api/";
+
+    // Create logger
+    private static HttpLoggingInterceptor logger =
+            new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    // Create OkHttp client
+    private static OkHttpClient.Builder okhttp =
+            new OkHttpClient.Builder().addInterceptor(logger);
+
+    private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okhttp.build());
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <S> S buildService(Class <S> serviceType){
+        return retrofit.create(serviceType);
+    }
 }
